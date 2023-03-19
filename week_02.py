8fbe82561c24863478288266c339933148d832d5
KLI0273 <126114680+KLI0273@users.noreply.github.com>
2023-03-19 12:42:20 +0100

    ukol02

--- week_02.py
+++ week_02.py
@@ -9,11 +9,22 @@ def odmocnina(a, n): #a = cislo #* n = pocet opakovani cyklu
         x = ((a/x)+x)/2 
     return x      
 
+def aproximace_pi(N):
+    b = 1
+    for i in range(N):
+        v = odmocnina(1**2 - (b/2)**2, 100)
+        S = 6*b*(v/2)
+        if(i>0): S = S*(2**i)
+        b = odmocnina((b/2)**2 + (1-v)**2,100)
+        if(b<0): b *= -1 
+        if(b == 0): return 'error'
 
-a = 554816
-n = 50
+    return S
 
+p = aproximace_pi(8)
+print(p)
+print(ma.pi)
 
-x = odmocnina(a,n)
-print(x)
-print(ma.sqrt(a))
+#v = odmocnina(a**2 - (a/2)**2, 100)
+#S = 6*a*(v/2)
+#a = odmocnina((a/2)**2 + (1-v)**2,100)
