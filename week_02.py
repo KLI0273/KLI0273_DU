c6bab8881a846edc17cde80b8ee5bf250da56aba
KLI0273 <126114680+KLI0273@users.noreply.github.com>
2023-03-19 22:28:54 +0100

    Ukol_03 done

--- week_02.py
+++ week_02.py
@@ -13,20 +13,22 @@ def aproximace_pi(N):
     for i in range(N):
         v = odmocnina(1**2 - (b/2)**2, 100)
         S = 6*(b*(v/2))*(2**i)
-        b = odmocnina((b/2)**2 + (1-v)**2,100) #max 97, pro vyzsi n-uhelníky je potreba vice iteraci
+        b = odmocnina((b/2)**2 + (1-v)**2,100) #max 97, pro vetsi pocet n-uhelniku je potreba vice iteraci
         if(b == 0): return 'error'
     return S
 
 def aproximace_pi_newton(n):
     a = 1/16
+    sum = 0
     for i in range(1,n+1):
-        sum = (a/(2*i+1))
+        sum += (a/(2*i+1))
         a = a*((2*(i+1)-3)/(2*(i+1)*4))
+
     m = odmocnina(3,1000)
     q = 12*(-(m/8) + 1/2 - sum)
     return q
     
-q = aproximace_pi_newton(10)
+q = aproximace_pi_newton(100)
 p = aproximace_pi(97)
 print(q)
 print('------------------')
