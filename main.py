e71dcfa4f127b3d65c243a37656164fcb77f1932
KLI0273 <126114680+KLI0273@users.noreply.github.com>
2023-03-28 18:46:40 +0200

    week_04_commit_01

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
-C = np.zeros((8,8))
-for i in range(len(C)):
-    if(i%2 == 0):C[i][0:8:2]= 1
-    if(i%2 == 1):C[i][1:7:2]= 1
-print(C)
+
+
+a = np.array([[1, 2], [3, 4]])
+
+
+print(np.tile(a, (3, 2)))
