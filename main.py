02f1d4ab0764a9e92fe8afe3f7b92faa19f365f0
KLI0273 <126114680+KLI0273@users.noreply.github.com>
2023-03-30 04:20:29 +0200

    week_04_Ukoly\Bonus (Finished)

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ def mandelbrot_set(x_min = -2, x_max = 1, y_min = -1.5, y_max = 1.5, n = 1000, k
     
     y, x = np.mgrid[x_min:x_max:complex(0, n), y_min:y_max:complex(0, n)]#.astype(np.float128)
     c = (x-0.5)+(y+0.5)* 1j
-    divergence_matrix = np.zeros((np.shape(c)))+k
+    divergence_matrix = np.zeros((np.shape(c))) +k
     zn = 0
     for i in range(k):
         zn = (zn**2) + c
         for m in range(n):
             for l in range(n):
-                if(np.abs(zn[m,l] > 2)):
+                if(np.abs(zn[m, l]) > 2):
 
                     divergence_matrix[m,l] -= 1
                     zn[m,l] = 2
          
     return divergence_matrix
 
-n = 100
+n = 200
 k = 100
 
 divergence_matrix = mandelbrot_set(n=n, k=k)
