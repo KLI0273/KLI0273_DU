a0bdbe71fe07fc26bf0e1cf6a1526da9a428d64d
KLI0273 <126114680+KLI0273@users.noreply.github.com>
2023-04-15 22:59:29 +0200

    Ukol_05_cvic_02_finished

--- test.py
+++ test.py
@@ -1,23 +1,6 @@
-
 import numpy as np
-from scipy.sparse import csr_matrix, csc_matrix, coo_matrix, lil_matrix
 
-data = np.array([1, 2, 4, 5, 6])
-row_indices = np.array([0, 0, 2, 2, 2])
-col_indices = np.array([0, 2, 0, 1, 2])
-
-# což odpovídá plné matici
-full_matrix = np.array([[1, 0, 2],
-                        [0, 0, 0],
-                        [4, 5, 6]])
-# LIL matrix
-lil = lil_matrix((3, 3))
-lil[0, 0] = 1
-lil[0, 2] = 2
-lil[2, 0] = 4
-lil[2, 1] = 5
-lil[2, 2] = 6
-print("LIL format:")
-print("data:", lil.data)
-print("rows:", lil.rows)
-print(lil)
+A = np.array(([1,2,3],[4,5,6],[7,8,9]))*0
+n, s = 1,1
+A[n-1:n+2,s] = 1
+print(A)
