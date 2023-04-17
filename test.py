2242ac72b4b49f81b0f7441103fc3522d8128a23
KLI0273 <126114680+KLI0273@users.noreply.github.com>
2023-04-17 14:00:45 +0200

    ukol_05_dvojka_hotova

--- test.py
+++ test.py
@@ -1,36 +1,22 @@
-import numpy as np
 import scipy.sparse as sparse
+import numpy as np
+import matplotlib.pyplot as plt
+
+def vyp_eps(i,j):
+    odecti = 0
+    if(i==0 or j==0 or i==n-1 or j==n-1): odecti += epsilon
+    if((i==0 and j==0) or (i==0 and j==n-1) or (i==n-1 and j==0) and (i==n-1 and j==n-1)): odecti += epsilon
+    return odecti
 
 n = 3
-epsilon = 0.1
+epsilon = 1e-1
+A = sparse.lil_matrix((n*n, n*n))
+for i in range(n):
+    for j in range(n):
+        if(j+n+(n*i)<n*n): A[j+(n*i),j+n+(n*i)] = -epsilon
+        if(j+n+(n*i)<n*n): A[j+n+(n*i), j+(n*i)] = -epsilon
+        if(i != n-1): A[n*j+i,n*j+1+i] = -epsilon
+        if(i != n-1): A[n*j+1+i,n*j+i] = -epsilon
+        A[i*n+j,i*n+j] = ((1+epsilon*4) - vyp_eps(i,j) )
 
-col1 = np.arange(1, n*n-n+1) + n-1
-row1 = np.arange(0, n*n-n)
-col2 = np.arange(1, n*n-n+1)-1
-row2 = np.arange(0, n*n-n) +n
-
-data = -epsilon * np.ones((n * n - n)*2)
-row = np.vstack((row1, row2)).reshape((-1,), order='F')
-col = np.vstack((col1, col2)).reshape((-1,), order='F')
-
-csr = sparse.csr_matrix((data, (row, col)))
-print(csr.todense())
-
-
-"""
-col1 = np.arange(1, n*n-n+1) + n-1
-row1 = np.arange(0, n*n-n)
-col2 = np.arange(1, n*n-n+1)-1
-row2 = np.arange(0, n*n-n) +n
-
-data = -epsilon * np.ones((n * n - n)*2)
-row = np.vstack((row1, row2)).reshape((-1,), order='F')
-col = np.vstack((col1, col2)).reshape((-1,), order='F')
-
-csr = sparse.csr_matrix((data, (row, col)))
-print(csr.todense())
-
-print(row_indices)
-print(col_indices)
-print(data)
-"""
+print(A.todense())
